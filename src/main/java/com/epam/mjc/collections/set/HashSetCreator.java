0df24f1a78221c2e07963f7e5f996afdcbbdfec8
SardorAKayumov <sardor.zeroz@gmail.com>
2022-11-26 18:50:14 +0500

    commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,20 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (Integer num : sourceList) {
+            if (num % 2 == 0) {
+                hashSet.add(num);
+                while (num % 2 == 0) {
+                    num /= 2;
+                    hashSet.add(num);
+                }
+            } else {
+                hashSet.add(num);
+                hashSet.add(num*2);
+            }
+        }
+
+        return hashSet;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,20 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> set = new HashSet<>();
+
+        // Belong to first and second sets but not to third
+        for (String str : firstSet) {
+            if (secondSet.contains(str) && !thirdSet.contains(str))
+                set.add(str);
+        }
+
+        // Unique element from third set
+        for (String str : thirdSet) {
+            if (!firstSet.contains(str) && !secondSet.contains(str))
+                set.add(str);
+        }
+
+        return set;
     }
 }
